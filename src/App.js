8ff40cef6bd72ef1b527d1f2d25fb6dbfd1f8cab
ilyas filali <ilyas.rafiqfilali@gmail.com>
2022-08-29 13:52:04 +0100

    initializing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <code><h2 data-text="Nayetwolf.">  
+          <code>Nayetwolf.</code> 
+        </h2></code>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://digital-business-card-ilyasfilali.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Digital Business-Card
         </a>
+
       </header>
     </div>
   );
 }
 
 export default App;
+
